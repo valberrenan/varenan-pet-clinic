adda1cbcff5d38dedab96d178aabf28de5775120
“Valber” <valber.lrenan@mercadolivre.com>
2021-07-05 14:49:46 -0300

    projeto iniciado

---
+++ src/main/java/com/example/petclinicvb/PetClinicVbApplication.java
@@ -0,0 +1,13 @@
+package com.example.petclinicvb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicVbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicVbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petclinicvb/PetClinicVbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petclinicvb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicVbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
